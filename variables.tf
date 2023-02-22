df9dac1b88e3c1c277008aa9832cc1f3b2725d26
Denys Vasyliev <den.vasyliev@gmail.com>
2023-02-22 00:09:52 +0000

    add more comments

--- variables.tf
+++ variables.tf
@@ -6,27 +6,29 @@ variable "GOOGLE_PROJECT" {
 variable "GOOGLE_REGION" {
   type        = string
   default     = "us-central1-c"
-  description = "GCP region name"
+  description = "GCP region to use"
 }
 
 variable "GKE_MACHINE_TYPE" {
   type        = string
   default     = "g1-small"
-  description = "machine type"
+  description = "Machine type"
 }
 
 variable "GKE_NUM_NODES" {
   type        = number
   default     = 2
-  description = "num nodes in pull"
+  description = "GKE nodes number"
 }
 
 variable "GKE_CLUSTER_NAME" {
-  type    = string
-  default = "main"
+  type        = string
+  default     = "main"
+  description = "GKE cluster name"
 }
 
 variable "GKE_POOL_NAME" {
-  type    = string
-  default = "main"
+  type        = string
+  default     = "main"
+  description = "GKE pool name"
 }
